22b9f4f9767833e01aff80dd8fce9fa7542c9113
Peter Steinberg <objectliteral@outlook.com>
2019-05-01 20:01:18 +0200

    Step 1

---
+++ main.js
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import ReactDom from 'react-dom';
+
+class CounterDisplay extends React.Component {
+    render() {
+        return (
+            <div>Counter is {this.props.counter}</div>
+        )
+    }
+}
+
+// const CounterDisplay = function ({counter}) {
+//     return (
+//         <div>Counter is {counter}</div>
+//     )
+// }
+
+const CounterButton = function ({count}) {
+    return (
+        <button onClick={count}>Count</button>
+    )
+}
+
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            counter: 0
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <CounterDisplay counter={this.state.counter} />
+                <CounterButton count={() => this.setState((state) => { return { counter: state.counter + 1 }; })} />
+            </div>
+        )
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    ReactDom.render(<App />, document.getElementById('root'));
+});
