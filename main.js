0101f1ecb024ed88a29139170cf2087279850291
Peter Steinberg <objectliteral@outlook.com>
2019-05-03 00:33:53 +0200

    Cleanup

--- main.js
+++ main.js
@@ -3,57 +3,7 @@ import ReactDom from 'react-dom';
 
 import pascalCase from 'just-pascal-case';
 
-function completeAssign(target, ...sources) {
-    sources.forEach(source => {
-      let descriptors = Object.keys(source).reduce((descriptors, key) => {
-        descriptors[key] = Object.getOwnPropertyDescriptor(source, key);
-        return descriptors;
-      }, {});
-      // by default, Object.assign copies enumerable Symbols too
-      Object.getOwnPropertySymbols(source).forEach(sym => {
-        let descriptor = Object.getOwnPropertyDescriptor(source, sym);
-        if (descriptor.enumerable) {
-          descriptors[sym] = descriptor;
-        }
-      });
-      Object.defineProperties(target, descriptors);
-    });
-    return target;
-  }
-
-const store = (function () {
-
-    const values = {};
-    const callbacks = {};
-
-    return {
-        subscribe (name, callback) {
-            if (typeof callbacks[name] === 'undefined') {
-                this.create(name);
-            }
-            callbacks[name].push(callback)
-        },
-        unsubscribe (name, callback) {
-            if (typeof callbacks[name] === 'undefined') {
-                callbacks[name] = [];
-            }
-            callbacks[name].splice(callbacks[name].indexOf(callback), 1);
-        },
-        create (name, initialValue) {
-            values[name] = initialValue;
-            callbacks[name] = [];
-            completeAssign(this, {
-                get [name] () {
-                    return values[name];
-                },
-                set [name] (value) {
-                    values[name] = value;
-                    callbacks[name].forEach((callback) => callback({[name]: value}));
-                }
-            });
-        }
-    };
-}());
+import store from './store';
 
 const withStore = function (...propNames) {
     return function (Component) {
@@ -116,12 +66,6 @@ class CounterButton extends React.Component {
         )
     }
 }
-
-store.create('counter', 0);
-
-// const CounterButtonWithStore = withStore('counter')(CounterButton);
-
-console.log(store);
 
 const App = function () {
     return (
