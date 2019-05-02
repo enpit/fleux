b643cdc428e4423738ec47ada36d38a31b21a751
Peter Steinberg <objectliteral@outlook.com>
2019-05-02 23:26:06 +0200

    Cleanup

--- main.js
+++ main.js
@@ -1,59 +1,7 @@
 import * as React from 'react';
 import ReactDom from 'react-dom';
 
-// import store, { withStore, } from './store';
 import { connect, createStore, withState } from './lib';
-
-// const { connect, withContext, withState, withStore } = (function () {
-
-//     var context, setContext, store;
-
-//     return {
-//         connect (Component, value) {
-//             context = React.createContext();
-//             return class extends React.Component {
-//                 render() {
-//                     return (
-//                         <context.Provider value={value}>
-//                             <Component />
-//                         </context.Provider>
-//                     )
-//                 }
-//             }
-//         },
-//         withContext (Component) {
-//             return function () {
-//                 return (
-//                     <context.Consumer>
-//                         {value => (<Component context={value} />)}
-//                     </context.Consumer>
-//                 )
-//             }
-//         },
-//         withStore (store, ...propNames) {
-//             if (propNames.every((propName) => typeof propName === 'string')) {
-//                 return readWriteHOC(store, propNames, propNames);
-//             } else if (propNames.length <= 2 && propNames.every((propName) => Array.isArray(propName))) {
-//                 return readWriteHOC(store, propNames[0], propNames[1] || []);
-//             }
-//         },
-//         withState (...propNames) {
-//             return function (Component) {
-//                 return function () {
-//                     const ComponentWithContext = withContext(function ({context}) {
-//                         const ComponentWithStore = withStore(context, ...propNames)(Component);
-//                         return (
-//                             <ComponentWithStore />
-//                         )
-//                     });
-//                     return (
-//                         <ComponentWithContext />
-//                     )
-//                 }
-//             }
-//         }
-//     }
-// }());
 
 const CounterDisplay = function ({counter}) {
     return (
@@ -61,7 +9,6 @@ const CounterDisplay = function ({counter}) {
     )
 }
 
-// const CounterDisplayWithStore = withStore(['counter'])(CounterDisplay);
 const CounterDisplayWithState = withState('counter')(CounterDisplay);
 
 const CounterButton = function ({setCounter}) {
@@ -70,7 +17,6 @@ const CounterButton = function ({setCounter}) {
     )
 }
 
-// const CounterButtonWithStore = withStore([], ['counter'])(CounterButton);
 const CounterButtonWithState = withState('counter')(CounterButton);
 
 // console.log(store);
--- store.js
+++
@@ -1,8 +0,0 @@
-import { createStore } from './lib';
-
-const store = createStore({
-    counter: 0
-});
-
-export const { withStore } = store;
-export default store;
