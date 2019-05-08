234d77e6f52d4667bae9c61a286d370cba303754
Peter Steinberg <objectliteral@outlook.com>
2019-05-08 13:36:51 +0200

    Move counter example to subfolder

---
+++ examples/counter/main.js
@@ -0,0 +1,37 @@
+import * as React from 'react';
+import ReactDom from 'react-dom';
+
+import { connect, withState } from '../../src';
+
+const CounterDisplay = function ({counter}) {
+    return (
+        <div>Counter is {counter}</div>
+    )
+}
+
+const CounterDisplayWithState = withState('counter')(CounterDisplay);
+
+const CounterButton = function ({setCounter}) {
+    return (
+        <button onClick={() => setCounter((counter) => counter+1)}>Count</button>
+    )
+}
+
+const CounterButtonWithState = withState('counter')(CounterButton);
+
+const App = function () {
+    return (
+        <div>
+            <CounterDisplayWithState />
+            <CounterButtonWithState />
+        </div>
+    )
+}
+
+const Root = connect(App, {
+    counter: 0
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    ReactDom.render(<Root />, document.getElementById('root'));
+});
