0eb18ccccab275d231ab27fa5fa31de8510834ef
Peter Steinberg <objectliteral@outlook.com>
2019-05-10 16:41:46 +0200

    Change store API methods to not be enumerable
    With `subscribe` etc. not being enumerable, users can loop over the store to easily access all user data in the store.

--- src/store.js
+++ src/store.js
@@ -4,34 +4,50 @@ const createStore = function (initialValues = {}) {
 
     const values = {};
     const callbacks = {};
+    const store = {};
 
-    const store = {
-        subscribe (name, callback) {
-            if (typeof callbacks[name] === 'undefined') {
-                this.create(name, this[name]);
-            }
-            callbacks[name].push(callback)
-        },
-        unsubscribe (name, callback) {
-            if (typeof callbacks[name] === 'undefined') {
-                callbacks[name] = [];
-            }
-            callbacks[name].splice(callbacks[name].indexOf(callback), 1);
-        },
-        create (name, initialValue) {
-            values[name] = initialValue;
-            callbacks[name] = [];
-            completeAssign(this, {
-                get [name] () {
-                    return values[name];
-                },
-                set [name] (value) {
-                    values[name] = value;
-                    callbacks[name].forEach((callback) => callback({[name]: value}));
-                }
-            });
+    const subscribe = function (name, callback) {
+        if (typeof callbacks[name] === 'undefined') {
+            this.create(name, this[name]);
         }
+        callbacks[name].push(callback)
     };
+
+    const unsubscribe = function (name, callback) {
+        if (typeof callbacks[name] === 'undefined') {
+            callbacks[name] = [];
+        }
+        callbacks[name].splice(callbacks[name].indexOf(callback), 1);
+    };
+
+    const create = function (name, initialValue) {
+        values[name] = initialValue;
+        callbacks[name] = [];
+        completeAssign(this, {
+            get [name] () {
+                return values[name];
+            },
+            set [name] (value) {
+                values[name] = value;
+                callbacks[name].forEach((callback) => callback({[name]: value}));
+            }
+        });
+    };
+
+    Object.defineProperties(store, {
+        subscribe: {
+            enumerable: false,
+            value: subscribe
+        },
+        unsubscribe: {
+            enumerable: false,
+            value: unsubscribe
+        },
+        create: {
+            enumerable: false,
+            value: create
+        }
+    });
 
     Object.entries(initialValues).forEach(([name, value]) => store.create(name, value));
 
