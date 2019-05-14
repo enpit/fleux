d9dc3d77e2e1c5a9343508bddb4842550441a0cd
Peter Steinberg <objectliteral@outlook.com>
2019-05-15 00:20:52 +0200

    Add test for conflicting keys causing an error
    Tests for the situation described in #3.
    
    Contains a slightly longer code comment regarding a different technical issue!

--- tests/hoc.test.js
+++ tests/hoc.test.js
@@ -3,6 +3,22 @@ const { mount, render, shallow } = require('enzyme');
 
 const { createStore, connect, withState } = require('../dist/index.cjs');
 
+/*
+ * *Breaths in* Okay.
+ * This was added because when React encounters an error while rendering a component it logs a lot of stuff to the console. This is fine in principle, but in here I have some tests that provoke such errors and I don't want to clutter the testing output with pointless stack traces - these are intentional errors after all.
+ * The problem is, of course, that mocking away `console.error` surpresses **all** error messages, even those that might not be intentionally provoked.
+ * The issue was discussed in https://github.com/facebook/react/issues/11098 and a fix to React was submitted that allows for specific conditions to make React surpress those error messages. A solution that provides these conditions can be found in https://gist.github.com/gaearon/adf9d5500e11a4e7b2c6f7ebf994fe56. Weirdly, nobody seemed to have bothered to put that into a Jest matcher, even though that idea was explicitly stated. :shrug:
+ * For the time being, I will use this to surpress the noise even though I don't like it.
+ */
+beforeEach(() => {
+    jest.spyOn(console, 'error')
+    global.console.error.mockImplementation(() => {})
+});
+
+afterEach(() => {
+    global.console.error.mockRestore()
+});
+
 describe('connect', function () {
     it('should accept a component and an object', function () {
 
@@ -142,5 +158,26 @@ describe('global state', function () {
         const AppWithState = connect(App, {foo: 'bar'});
         const wrapper = mount(<AppWithState />);
         expect(wrapper.find(Foo).text()).toBe('42');
+    });
+
+    it('should throw an error if a prop name that is passed to a stateful component conflicts with a key from the store', function () {
+        const Foo = function ({foo}) {
+            return (
+                <div>{foo}</div>
+            )
+        }
+
+        const FooWithState = withState('foo')(Foo);
+
+        const App = function () {
+            return (
+                <FooWithState foo={'bar'} />
+            )
+        }
+
+        const AppWithState = connect(App, {foo: 'bar'});
+        expect(() => {
+            const wrapper = mount(<AppWithState />);
+        }).toThrow();
     });
 });
