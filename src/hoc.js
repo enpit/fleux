bea52a75b7391e3b445ce103c43484fea189b0a0
Peter Steinberg <objectliteral@outlook.com>
2019-05-15 19:48:38 +0200

    Fix issues with parsing `withState`’s arguments
    Fixes #8

--- src/hoc.js
+++ src/hoc.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import pascalCase from 'just-pascal-case';
+import typeOf from 'just-typeof';
 import fromEntries from 'fromentries';
 
 import { createStore, isStore } from './store';
@@ -86,30 +87,45 @@ const withStore = function (store, ...propNames) {
     if (propNames.every((propName) => typeof propName === 'string')) {
         return readWriteHOC(store, propNames, propNames);
     } else if (propNames.length <= 2 && propNames.every((propName) => Array.isArray(propName))) {
-        return readWriteHOC(store, propNames[0], propNames[1] || []);
+        return readWriteHOC(store, propNames[0] || [], propNames[1] || []);
     }
 }
 
 const withState = function (...propNames) {
+
+    const parsedProps = parseProps(propNames);
+
     return function (Component) {
         return function (props) {
 
-            const conflictingNames = propNames.filter(name => props.hasOwnProperty(name));
+            const conflictingNames = parsedProps.flat().filter(name => props.hasOwnProperty(name));
 
             if (conflictingNames.length > 0) {
                 throw Error(`Refusing to overwrite store props with parent-injected prop. The name(s) ${conflictingNames} exist in the store and are passed down from the parent component, resulting in a naming conflict.`);
             }
 
             const ComponentWithContext = withContext(function ({context, ...props}) {
-                const ComponentWithStore = withStore(context, ...propNames)(Component);
+                const ComponentWithStore = readWriteHOC(context, ...parsedProps)(Component);
                 return (
                     <ComponentWithStore {...props} />
-                )
+                );
             });
+
             return (
                 <ComponentWithContext {...props} />
-            )
+            );
+
         }
+    }
+}
+
+const parseProps = function (propNames) {
+    if (propNames.every((propName) => typeof propName === 'string')) {
+        return [ propNames, propNames ];
+    } else if (propNames.length <= 2 && propNames.every((propName) => Array.isArray(propName) || typeOf(propName) === 'null')) {
+        return [ propNames[0] || [], propNames[1] || [] ];
+    } else {
+        throw Error(`Failed to parse props. Rejected arguments of the types ${propNames.map(propName => typeof propName)}`);
     }
 }
 
