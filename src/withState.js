1b84b11f3b35832fefa243e189f91ac91c4530e9
Peter Steinberg <objectliteral@outlook.com>
2019-09-12 01:24:20 +0200

    Refactor parsing of `withState` arguments
    Now, a `mapStateToProps` equivalent is supported as the first argument to `withState`. It also internally restructures the interaction between `parseProps` and `withState`.
    
    This commit has multiple consequences, but it did not seem possible to separate this into more than one commit:
    
    1) `withState` now supports a first parameter that is interpreted as a `mapStateToProps` equivalent: If it is a function, it is called with the store and the component's own props, and returns an object with prop names and corresponding values.
    2) `withState` no longer supports some of the former ways of supplying it with arguments. Most importantly, the concept of read/write keys no longer exists. This is a (late) consequence of the power of implicit bindings and the availability of actions.
    3) `withState` now supports prop renaming. This was previously attempted on a different branch, but was done again here to make the implementation consistent with other new features. When the first argument to `withState` is an object then it is interpreted as a simple name mapping from store names to component local prop names. A renaming can also be done by supplying a function as the first argument to `withState` and have that function return an object that does the renaming.
    4) `parseProps` and `withState` now interact differently. `parseProps` no longer returns a list of prop names (and actions) but two functions: `selectStateProps` and `bindActionProps`. Both of these are called from within `withState` with the components individual store proxy. These functions bind props and actions to the store by calling the originally supplied `mapStateToProps` and `mapDispatchToProps` functions or intermediates that are created inside of `parseProps`.
    
    This is a breaking change.

--- src/withState.js
+++ src/withState.js
@@ -6,6 +6,7 @@ import fromEntries from 'fromentries';
 import { createStore } from './store';
 import context from './context';
 import * as SYMBOLS from './symbols';
+import preventWrites from './preventWrites';
 import parseProps from './parseProps';
 
 const defaultStore = createStore();
@@ -73,31 +74,18 @@ const withState = function (...args) {
 
     } else {
 
-        const [ readablePropNames, writeablePropNames, actions ] = parseProps(args);
+        const [ selectStateProps, bindActionProps ] = parseProps(args);
 
         return function (Component) {
 
             const ComponentWithState = function (props) {
 
-                const conflictingNames = readablePropNames.filter(name => props.hasOwnProperty(name)).concat(writeablePropNames.filter(name => props.hasOwnProperty('set' + pascalCase(name))));
-
-                if (conflictingNames.length > 0) {
-                    throw Error(`Refusing to overwrite store props with parent-injected prop. The name(s) ${conflictingNames} exist in the store and are passed down from the parent component, resulting in a naming conflict.`);
-                }
-
                 const explicitlyBoundComponent = function ({store}) {
 
-                    const readableProps = fromEntries(readablePropNames.map((propName) => [propName, store[propName]]));
+                    const immutableStore = preventWrites(store, 'Refusing to write to store inside of mapStateToProps or mapDispatchToProps.');
 
-                    const writeableProps = fromEntries(writeablePropNames.map((propName) => [ 'set' + pascalCase(propName), (value) => {
-                        if (typeof value === 'function') {
-                            store[propName] = value(store[propName]);
-                        } else {
-                            store[propName] = value;
-                        }
-                    } ] ));
-
-                    const actionProps = actions(store, props);
+                    const stateProps = selectStateProps(immutableStore, props);
+                    const actionProps = bindActionProps(immutableStore, props);
 
                     const conflictingNames = Object.keys(actionProps || {}).filter(name => props.hasOwnProperty(name));
 
@@ -106,7 +94,7 @@ const withState = function (...args) {
                     }
 
                     return (
-                        <Component {...props} {...readableProps} {...writeableProps} {...actionProps} store={store} dispatch={store.dispatch} />
+                        <Component {...props} {...stateProps} {...actionProps} store={store} dispatch={store.dispatch} />
                     );
 
                 }
