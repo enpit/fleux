c5beb0b34d6e59af44cc0923e94870021f52be2f
Peter Steinberg <objectliteral@outlook.com>
2019-09-22 02:07:31 +0200

    Add hooks
    This commit adds a couple of React hooks that allow the use of fleux's features without the need for higher-order components. Two hooks are related to actions, two are related to receiving state:
    
    - `useDispatch` retrieves the store from context and returns that store's `dispatch` function
    - `useAction` retrieves the store from context and returns its named actions
    - `useStore` retrieves the store from context and sets up a proxy with a get trap (similar to how `withState` does it), and keeps an internal state to be able to trigger rerenders
     `useSelector` works like `useStore` but takes a `selectStateProps` function and applies it to the store, and passes only the resulting selection to the calling component

---
+++ src/hooks.js
@@ -0,0 +1,68 @@
+import { useContext, useState } from 'react';
+import compare from 'just-compare';
+
+import * as SYMBOLS from './symbols';
+import context from './context';
+import getNamedActions from './getNamedActions';
+import preventWrites from './preventWrites';
+
+export const useStore = function () {
+    const store = useContext(context);
+    var stateProps = {};
+
+    const callback = function (prop, value) {
+        if (!compare(stateProps[prop], value)) {
+            stateProps[prop] = value;
+            localState[1]({[prop]: value});
+        }
+    };
+
+    const localProxy = new Proxy(store, {
+        get: function (target, prop) {
+            return target[SYMBOLS.STORE_GET](prop, callback);
+        }
+    });
+
+    const localState = useState(localProxy);
+    stateProps = { ...localState[0] };
+
+    return localProxy;
+}
+
+export const useDispatch = function () {
+    return useContext(context).dispatch;
+}
+
+export const useActions = function () {
+    const store = useContext(context);
+    return getNamedActions(store);
+}
+
+export const useSelector = function (selectStateProps) {
+    const store = useContext(context);
+
+    var stateProps = {};
+
+    const callback = function (prop, value) {
+        const updatedStore = {...localProxy, createAction: localProxy.createAction, [prop]: value};
+        const immutableStore = preventWrites(updatedStore, 'Refusing to write to store inside of mapStateToProps.');
+        const updatedProps = selectStateProps(immutableStore);
+        if (!compare(stateProps, updatedProps)) {
+            stateProps = updatedProps;
+            localState[1]({[prop]:value});
+        }
+    };
+
+    const localProxy = new Proxy(store, {
+        get: function (target, prop) {
+            return target[SYMBOLS.STORE_GET](prop, callback);
+        }
+    });
+
+    const localState = useState(localProxy);
+
+    const immutableStore = preventWrites(localState[0], 'Refusing to write to store inside of mapStateToProps.');
+    stateProps = selectStateProps(immutableStore);
+
+    return { ...stateProps, dispatch: store.dispatch };
+}
