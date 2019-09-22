b7421b841900102087f816dfba3c7cad68c2693e
Peter Steinberg <objectliteral@outlook.com>
2019-09-22 02:20:12 +0200

    Add exports of new features

--- src/index.js
+++ src/index.js
@@ -2,3 +2,5 @@ export { connect } from './connect';
 export { withState } from './withState';
 export { createStore, isStore } from './store';
 export { prepareWriteActions } from './prepareWriteActions';
+export { getNamedActions } from './getNamedActions';
+export { useActions, useDispatch, useSelector, useStore } from './hooks';
